e18ae76fbefe3f837b06b96f0de64f05a88ca3a7
muhammed ramshin <muhammedramshin@gmail.com>
2020-04-30 20:28:52 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function change() {
+
+    var x = document.getElementById("p1");
+    var y = document.getElementById("p2");
+    var z = document.getElementById("p3");
+    var a = document.getElementById("p4");
+    if (x.className === "profile" && y.className === "details bold" && z.className === "links" &&  a.className === "details" ) {
+      
+        x.className += " dark";
+        
+      y.className += " dark";
+      z.className += " dark";
+      a.className += " dark";
+      
+      document.body.style.backgroundColor = "black";
+      document.body.style.transition="1s ease-in-out";
+    } else {
+      x.className = "profile";
+      y.className = "details bold";
+      z.className = "links";
+      a.className="details"
+      document.body.style.backgroundColor = "white";
+      document.body.style.transition="1s ease-in-out";
+    }
+
+    
+
+  }
